5b63906d13c4ad8fb2a1710adf771753c4fe8560
postmaduyen <leanhduc281003@gmail.com>
2025-08-12 17:16:49 +0700

    url

--- src/main/java/com/spring/LibraryManagement/LibraryManagementApplication.java
+++ src/main/java/com/spring/LibraryManagement/LibraryManagementApplication.java
@@ -2,9 +2,10 @@ package com.spring.LibraryManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableScheduling
 public class LibraryManagementApplication {
 	public static void main(String[] args) {
