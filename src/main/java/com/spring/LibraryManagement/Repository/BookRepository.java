2ea5be6af72f71fc47994325696fc70bb00c72b7
postmaduyen <leanhduc281003@gmail.com>
2025-08-13 15:07:55 +0700

    del url

--- src/main/java/com/spring/LibraryManagement/Repository/BookRepository.java
+++ src/main/java/com/spring/LibraryManagement/Repository/BookRepository.java
@@ -3,11 +3,10 @@ package com.spring.LibraryManagement.Repository;
 import com.spring.LibraryManagement.Entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import javax.swing.text.html.Option;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long > {
     @Query("SELECT b FROM Book b JOIN Borrow br ON b.id = br.book.id WHERE br.id = :borrowId")
     Optional<Book> findBooksByBorrowId(Long borrowId);
